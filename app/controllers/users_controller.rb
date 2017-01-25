ef0e3f77a6e4b468385901da60b0d1a7d3e4ce21
Mayra Cabrera <mcabrera1087@gmail.com>
2017-01-24 19:27:29 -0600

    Build relationships between user & company through accep_nested_attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,25 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @user.build_company
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, company_attributes: [:name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  belongs_to :company
+
+  accepts_nested_attributes_for :company
 end
